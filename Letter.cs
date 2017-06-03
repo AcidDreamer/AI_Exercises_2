a33dfb42aea0c59fd837a1a93a3e4c643739886e
AcidDreamer <nmandrelas@gmail.com>
2017-06-03 13:31:42 +0300

    added comments

--- Letter.cs
+++ Letter.cs
@@ -10,12 +10,12 @@ namespace Exercise_2_1 {
         public static int integers_size = 16;
         public String letter { get; set; }
         public int[] integers { get; set; }
-        
+        //constructor
         public Letter(string letter,int[] integers) {
             this.letter = letter;
             this.integers = integers;
         }
-
+        //create a flexible toString method
         public override string ToString() {
             PropertyDescriptorCollection coll = TypeDescriptor.GetProperties(this);
             StringBuilder builder = new StringBuilder();

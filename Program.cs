9342814abbd73237933bb0e7973d15ce202452fe
AcidDreamer <nmandrelas@gmail.com>
2017-06-03 13:34:51 +0300

    Added comments

--- Program.cs
+++ Program.cs
@@ -7,17 +7,27 @@ using System.Threading.Tasks;
 namespace Exercise_2_1 {
     class Program {
         static void Main(string[] args) {
+            
+            break_sets bs = new break_sets();
             DataLoader dl = new DataLoader();
+            //get an instance of both algorithms
             KNN knn = new KNN();
             ID3 id3 = new ID3();
-            break_sets bs = new break_sets();
+            //load the complete data set
             List<Letter> letters =  dl.return_data();
+            //get the training set
             List<Letter> training = bs.return_training_set(letters);
+            //get the validation set
             List<Letter> test = bs.return_validation_set(letters);
-            Console.WriteLine(knn.Accord_Knn(test, training, 55));
+            //Run KNN , 5 closest neighbors
+            Console.WriteLine(knn.Accord_Knn(test, training, 5));
+            //Run KNN , 10 closest neighbors
             Console.WriteLine(knn.Accord_Knn(test, training, 10));
+            //Run KNN , 25 closest neighbors
             Console.WriteLine(knn.Accord_Knn(test, training, 25));
+            //Run ID3
             Console.WriteLine(id3.Accord_ID3(test, training));
+            //Pause to read the data
             Console.ReadLine();
         }
     }
